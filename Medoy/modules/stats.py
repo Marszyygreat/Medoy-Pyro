73d58f418ddd91eee5fe48246a8bbbeff86ba92f
Marszyygreat <159394182+Marszyygreat@users.noreply.github.com>
2024-03-03 06:49:00 +0700

    Update stats.py

--- Medoy/modules/stats.py
+++ Medoy/modules/stats.py
@@ -13,7 +13,7 @@ from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 
 from config import CMD_HANDLER as cmd
-from ProjectMan.helpers.basic import edit_or_reply
+from Medoy.helpers.basic import edit_or_reply
 
 from .help import *
 
