0ea61aaacbb4530b7cee94b6bc0bef7a9fcf76a3
Marszyygreat <159394182+Marszyygreat@users.noreply.github.com>
2024-03-02 23:21:48 +0700

    Create __main__.py

---
+++ Medoy/__main__.py
@@ -0,0 +1,59 @@
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+import importlib
+
+from pyrogram import idle
+from uvloop import install
+
+from config import BOT_VER, CMD_HANDLER
+from Medoy import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
+from Medoy.helpers.misc import create_botlog, heroku
+from Medoy.modules import ALL_MODULES
+
+MSG_ON = """
+🔥 **Medoy-Userbot Berhasil Di Aktifkan**
+━━
+➠ **Userbot Version -** `{}`
+➠ **Ketik** `{}alive` **untuk Mengecheck Bot**
+━━
+"""
+
+
+async def main():
+    for all_module in ALL_MODULES:
+        importlib.import_module(f"Medoy.modules.{all_module}")
+    for bot in bots:
+        try:
+            await bot.start()
+            bot.me = await bot.get_me()
+            await bot.join_chat("Lunatic0de")
+            try:
+                await bot.send_message(
+                    BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER)
+                )
+            except BaseException:
+                pass
+            LOGGER("Medoy").info(
+                f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
+            )
+        except Exception as a:
+            LOGGER("main").warning(a)
+    LOGGER("Medoy").info(f"Medoy-UserBot v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+    if bot1 and not str(BOTLOG_CHATID).startswith("-100"):
+        await create_botlog(bot1)
+    await idle()
+    await aiosession.close()
+
+
+if __name__ == "__main__":
+    LOGGER("Medoy").info("Starting PyroMan-UserBot")
+    install()
+    heroku()
+    LOOP.run_until_complete(main())
