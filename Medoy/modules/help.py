74acdfc5310da6f2156956743db919b9ed755b52
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-03-07 08:13:36 +0700

    Update help.py

--- Medoy/modules/help.py
+++ Medoy/modules/help.py
@@ -21,39 +21,52 @@ from Medoy.helpers.utility import split_list
 async def module_help(client: Client, message: Message):
     cmd = message.command
     help_arg = ""
+    bot_username = (await app.get_me()).username
     if len(cmd) > 1:
         help_arg = " ".join(cmd[1:])
-    elif message.reply_to_message and len(cmd) == 1:
-        help_arg = message.reply_to_message.text
     elif not message.reply_to_message and len(cmd) == 1:
-        ac = PrettyTable()
-        ac.header = False
-        ac.title = "PyroMan-UserBot Modules"
-        ac.align = "l"
-        for x in split_list(sorted(CMD_HELP.keys()), 2):
-            ac.add_row([x[0], x[1] if len(x) >= 2 else None])
-        await edit_or_reply(
-            message, f"```{str(ac)}```\n• MEDOY - USERBOT •"
-        )
-        await message.reply(
-            f"**Contoh Ketik** `{CMD_HANDLER}help afk` **Untuk Melihat Informasi Module**"
-        )
+        try:
+            nice = await client.get_inline_bot_results(bot=bot_username, query="helper")
+            await asyncio.gather(
+                message.delete(),
+                client.send_inline_bot_result(
+                    message.chat.id, nice.query_id, nice.results[0].id
+                ),
+            )
+        except BaseException as e:
+            print(f"{e}")
+            ac = PrettyTable()
+            ac.header = False
+            ac.title = "Medoy Userbot Plugins"
+            ac.align = "l"
+            for x in split_list(sorted(CMD_HELP.keys()), 2):
+                ac.add_row([x[0], x[1] if len(x) >= 2 else None])
+            xx = await client.send_message(
+                message.chat.id,
+                f"```{str(ac)}```\n• Medoy Userbot •",
+                reply_to_message_id=ReplyCheck(message),
+            )
+            await xx.reply(
+                f"**Usage:** `{cmds}help broadcast` **To View Module Information**"
+            )
+            return
 
     if help_arg:
         if help_arg in CMD_HELP:
             commands: dict = CMD_HELP[help_arg]
-            this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
+            this_command = f"**Help For {str(help_arg).upper()}**\n\n"
             for x in commands:
-                this_command += f"  •  **Command:** `{CMD_HANDLER}{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Lunatic0de"
+                this_command += f"**Command:** `{str(x)}`\n**Function:** `{str(commands[x])}`\n\n"
+            this_command += "© @GeezRam - @UserbotCh"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
         else:
             await edit_or_reply(
                 message,
-                f"`{help_arg}` **Bukan Nama Modul yang Valid.**",
+                f"`{help_arg}` **tidak ada dalam list modul.**",
             )
+
 
 
 def add_command_help(module_name, commands):
