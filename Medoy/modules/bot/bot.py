ab0dd74f0eb42bc2bc5def0a118b9987f2d1c826
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-03-07 11:08:53 +0700

    Update bot.py

--- Medoy/modules/bot/bot.py
+++ Medoy/modules/bot/bot.py
@@ -6,7 +6,7 @@ from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMa
 
 from Medoy.helpers.data import Data
 from Medoy import CMD_HELP, app
-from Medoy.helpers.inline import cb_wrapper, paginate_help
+from Medoy.helpers.inline import paginate_help
 from Medoy import ids as users
 from Medoy import *
 
@@ -56,7 +56,6 @@ async def _callbacks(_, callback_query: CallbackQuery):
 
 
 @app.on_callback_query(filters.regex("ub_modul_(.*)"))
-@cb_wrapper
 async def on_plug_in_cb(_, callback_query: CallbackQuery):
     modul_name = callback_query.matches[0].group(1)
     commands: dict = CMD_HELP[modul_name]
@@ -80,7 +79,6 @@ async def on_plug_in_cb(_, callback_query: CallbackQuery):
 
 
 @app.on_callback_query(filters.regex("reopen"))
-@cb_wrapper
 async def reopen_in_cb(_, callback_query: CallbackQuery):
     buttons = paginate_help(0, CMD_HELP, "helpme")
     await app.edit_inline_text(
@@ -91,7 +89,6 @@ async def reopen_in_cb(_, callback_query: CallbackQuery):
 
 
 @app.on_callback_query(filters.regex("helpme_prev\((.+?)\)"))
-@cb_wrapper
 async def on_plug_prev_in_cb(_, callback_query: CallbackQuery):
     current_page_number = int(callback_query.matches[0].group(1))
     buttons = paginate_help(current_page_number - 1, CMD_HELP, "helpme")
@@ -103,7 +100,6 @@ async def on_plug_prev_in_cb(_, callback_query: CallbackQuery):
 
 
 @app.on_callback_query(filters.regex("helpme_next\((.+?)\)"))
-@cb_wrapper
 async def on_plug_next_in_cb(_, callback_query: CallbackQuery):
     current_page_number = int(callback_query.matches[0].group(1))
     buttons = paginate_help(current_page_number + 1, CMD_HELP, "helpme")
