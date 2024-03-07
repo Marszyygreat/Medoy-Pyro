681dbc4fd1773cc5f6278f0f1d8e2dc67478128d
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-03-07 11:23:13 +0700

    Update data.py

--- Medoy/helpers/data.py
+++ Medoy/helpers/data.py
@@ -1,11 +1,11 @@
 from pyrogram.types import InlineKeyboardButton, WebAppInfo
-from Medoy import CMD_HELP
+from Medoy import CMD_HELP, CMD_HANDLER 
 
 class Data:
     
    num_basic_modules = len(CMD_HELP)
 
    text_help_menu = (
-        f"**Help Menu**"
+        f"**Help Menu**   "
     )
    reopen = [[InlineKeyboardButton("Open", callback_data="reopen")]]
