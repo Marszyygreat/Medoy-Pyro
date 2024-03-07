87534c5282331d7e98511ebdd16d5ba8b6f04cde
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-03-07 10:23:16 +0700

    Update __init__.py

--- Medoy/__init__.py
+++ Medoy/__init__.py
@@ -111,7 +111,7 @@ app = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="Medoy/modules"),
+    plugins=dict(root="Medoy/modules/bot"),
     in_memory=True,
 )
 
